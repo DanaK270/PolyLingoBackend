15bcccce19513e86f544dcb3a6c42276cd1cdb05
Danak270 <daanaa227700@gmail.com>
2024-11-06 20:46:27 +0300

    auth setup done.
    tested the login, signup, check session and update pass in insomnia and they all work
    
    NOTE : do npm i

--- server.js
+++ server.js
@@ -10,17 +10,22 @@ const PORT = process.env.PORT || 4000
 // Initialize Express
 const app = express()
 
-// app.use(cors())
 // app.use('/images', express.static(path.join(__dirname, '/public/images')))
 
-// const db = require('./config/db')
+// configure database
+const db = require('./config/db')
+
+// Middleware to parse JSON
+app.use(express.json())
 
 // Import Routes
+const AuthRouter = require('./routes/AuthRouter')
 
 // CORS Configuration
 // app.use(cors())
 
 // Mount Routes (after CORS)
+app.use('/auth', AuthRouter)
 
 // Start server
 app.listen(PORT, () => {
