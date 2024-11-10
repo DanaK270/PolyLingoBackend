29bf2dc2b94c6e500a5da27540a44f076dc874d6
Meryam313 <em.mohmed.24@gmail.com>
2024-11-10 12:48:17 +0300

    test

--- routes/userNote.js
+++ routes/userNote.js
@@ -1,20 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/userNote');
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/userNote')
 
-// Route to create a new user note
-router.post('/notes', controller.createUserNote);
+router.post('/notes', controller.createUserNote)
+router.get('/notes/user/:userId', controller.getUserNotesByUserId)
+router.get('/notes/:id', controller.getUserNoteById)
+router.put('/notes/:id', controller.updateUserNote)
+router.delete('/notes/:id', controller.deleteUserNote)
 
-// Route to get all notes for a specific user
-router.get('/notes/user/:userId', controller.getUserNotesByUserId);
-
-// Route to get a specific note by ID
-router.get('/notes/:id', controller.getUserNoteById);
-
-// Route to update a note by ID
-router.put('/notes/:id', controller.updateUserNote);
-
-// Route to delete a note by ID
-router.delete('/notes/:id', controller.deleteUserNote);
-
-module.exports = router;
+module.exports = router
