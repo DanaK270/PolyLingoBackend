4a32dd479a3668c82c4a0a8575b213777e97bceb
Dana <Danayousif867@gmail.com>
2024-11-10 10:01:21 +0300

    language and lesson mostly done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const expressLayouts = require('express-ejs-layouts')
 // const cors = require('cors')
 require('dotenv').config()
 // const path = require('path')
@@ -12,14 +13,21 @@ const app = express()
 
 // app.use(cors())
 // app.use('/images', express.static(path.join(__dirname, '/public/images')))
+app.use(express.urlencoded({ extended: true }));
+const db = require('./config/db')
+app.get('/', function (req, res) {})
+app.set('view engine', 'ejs')
 
 // const db = require('./config/db')
-
+const languageRouter=require("./routes/language")
 // Import Routes
 
 // CORS Configuration
 // app.use(cors())
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(expressLayouts)
+app.use("/language",languageRouter)
 // Mount Routes (after CORS)
 
 // Start server
