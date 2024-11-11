f274835d6593952f592f4c6e440c1d6b7c7de5f8
HafsaAlkhaja <hafsaalkhaja@gmail.com>
2024-11-11 15:40:39 +0300

    translation backend done

--- server.js
+++ server.js
@@ -7,32 +7,33 @@ const logger = require('morgan');
 const cors = require('cors')
 
 const expressLayouts = require('express-ejs-layouts')
-// const cors = require('cors')
+
+
+
+const translation = require('./routes/translation');
+
 
 require('dotenv').config()
-// const path = require('path')
-const { Reply } = require('./models/Reply');  // Adjust the path if necessary
+
+const { Reply } = require('./models/Reply');  
 
 
-// PORT Configuration
+
 const PORT = process.env.PORT || 4000
 
-// Initialize Express
+
 const app = express()
 
-// app.use('/images', express.static(path.join(__dirname, '/public/images')))
 app.use(express.urlencoded({ extended: true }));
 const db = require('./config/db')
 app.get('/', function (req, res) {})
 app.set('view engine', 'ejs')
 
 
-// configure database
+
 
 const { Issue } = require('./models/Issue');
 
-// Middleware to parse JSON
-// CORS Configuration
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
@@ -41,30 +42,25 @@ const issueRouter = require('./routes/issue')
 
 
 
-// const db = require('./config/db')
 const languageRouter=require("./routes/language")
 
-// Import Routes
 const AuthRouter = require('./routes/AuthRouter')
 
-
+app.use('/translate', translation);
 
 app.use(cors())
 
 
-
-// CORS Configuration
-// app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(expressLayouts)
 app.use("/language",languageRouter)
 
-// Mount Routes (after CORS)
+
 app.use('/auth', AuthRouter)
 app.use('/issues', issueRouter)
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`App is running on PORT ${PORT}`)
 })
